f5e5ce3dc2e49c068e5bf6d0c73dd7c1337e9919
Aayush Mittal <aayush.mittal@pinelabs.com>
2019-07-07 19:22:18 +0530

    Add comments to fluent builder

--- DesignPatterns/FluentBuilderPattern/FluentBuilderPattern.cs
+++ DesignPatterns/FluentBuilderPattern/FluentBuilderPattern.cs
@@ -4,21 +4,52 @@ using System.Text;
 
 namespace DesignPatterns.FluentBuilderPattern
 {
+    /// <summary>
+    /// Class to create Html Element like <html>, <head>
+    /// </summary>
     class HtmlElement
     {
-        public string Name, Text;
+        /// <summary>
+        /// The name of html element
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// The text of html element
+        /// </summary>
+        public string Text;
+
+        /// <summary>
+        /// The elements contained by root element
+        /// </summary>
         public List<HtmlElement> Elements = new List<HtmlElement>();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HtmlElement"/> class.
+        /// Constructor used to create the root element
+        /// </summary>
         public HtmlElement()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HtmlElement"/> class.
+        /// Constructor to create the child elements of the root elements
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="text">The text.</param>
         public HtmlElement(string name, string text)
         {
             this.Name = name;
             this.Text = text;
         }
 
+        /// <summary>
+        /// Print the Html formed
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
             var stringBuilder = new StringBuilder();
@@ -37,18 +68,38 @@ namespace DesignPatterns.FluentBuilderPattern
 
     }
 
+    /// <summary>
+    /// Html Builder class
+    /// </summary>
     class HtmlBuilder
     {
+        /// <summary>
+        /// The root element
+        /// </summary>
         HtmlElement root = new HtmlElement();
 
+        /// <summary>
+        /// The root element name
+        /// </summary>
         private string rootName;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HtmlBuilder"/> class.
+        /// Used to create root element of the html builder
+        /// </summary>
+        /// <param name="rootName">Name of the root.</param>
         public HtmlBuilder(string rootName)
         {
             this.rootName = rootName;
             root.Name = rootName;
         }
 
+        /// <summary>
+        /// Adds the child element to root.
+        /// </summary>
+        /// <param name="childName">Name of the child.</param>
+        /// <param name="childText">The child text.</param>
+        /// <returns>the html builder itself</returns>
         public HtmlBuilder AddChild(string childName, string childText)
         {
             var element = new HtmlElement(childName, childText);
@@ -56,6 +107,12 @@ namespace DesignPatterns.FluentBuilderPattern
             return this;
         }
 
+        /// <summary>
+        /// Print the string formed by html builder
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
             return root.ToString();
