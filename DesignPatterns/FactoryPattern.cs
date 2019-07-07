a84c1cfe619adb590c4754955febd58e7433f23d
Aayush Mittal <aayush.mittal@pinelabs.com>
2019-07-07 17:26:37 +0530

    Factory Pattern Implemented

---
+++ DesignPatterns/FactoryPattern.cs
@@ -0,0 +1,35 @@
+﻿// Factory Pattern 
+namespace DesignPatterns.FactoryPattern
+{
+    /// <summary>
+    /// Samsung class to demonstrate the factory pattern
+    /// </summary>
+    public class Samsung
+    {
+        private string product;
+
+        private Samsung(string product)
+        {
+            this.product = product;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(product)} : {product}";
+        }
+
+        public static class Factory
+        {
+            public static Samsung GetNewFridge()
+            {
+                return new Samsung("Here is your fridge");
+            }
+
+            public static Samsung GetNewWashingMachine()
+            {
+                return new Samsung("Here is your washing machine");
+            }
+        }
+    }
+}
+
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,19 @@
+﻿using DesignPatterns.FactoryPattern;
+using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fridge = Samsung.Factory.GetNewFridge();
+            var washingMachine = Samsung.Factory.GetNewWashingMachine();
+
+            Console.WriteLine(fridge.ToString());
+            Console.WriteLine(washingMachine.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
